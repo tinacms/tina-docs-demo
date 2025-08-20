ca9a84691043b141c49e0228f7b14cff56963007
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-08-21 08:55:13 +1000

    Initial commit from Create Tina App

---
+++ src/app/tina-client.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import type React from "react";
+import { useTina } from "tinacms/dist/react";
+
+export type UseTinaProps = {
+  query: string;
+  variables: Record<string, unknown>;
+  data: Record<string, unknown>;
+  forceExperimental?: string;
+};
+
+export type TinaClientProps<T> = {
+  props: UseTinaProps & T & any;
+  Component: React.FC<{
+    tinaProps: { data: Record<string, unknown> };
+    props: {
+      query: string;
+      variables: Record<string, unknown>;
+      data: Record<string, unknown>;
+      pageTableOfContents: Record<string, unknown>;
+      documentationData: Record<string, unknown>;
+    };
+  }>;
+};
+
+export function TinaClient<T>({ props, Component }: TinaClientProps<T>) {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+    experimental___selectFormByFormId() {
+      return `content/docs/${props.forceExperimental}`;
+    },
+  });
+
+  return <Component tinaProps={{ data }} props={{ ...props }} />;
+}
